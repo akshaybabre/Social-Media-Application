3226190043acdea42abae317285ecf73b93848b0
akshaybabre <akshaybabre8@gmail.com>
2025-07-01 10:53:02 +0530

    Updated New Feature Comment

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
-import LoginPage from "scenes/loginPage";
+import LoginPage from "./scenes/navbar/loginPage";
 import ProfilePage from "scenes/profilePage";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
